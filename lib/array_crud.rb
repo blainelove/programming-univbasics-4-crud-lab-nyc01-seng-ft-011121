a6c19a896b1a04e37485272f38b8fb40a71c3620
Blaine R Love <blainelove94@gmail.com>
2020-12-29 21:06:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  bill.push("item")
+  bill = [0, 1, 2, 3]
+  bill<<("item")
 end
 
 def add_element_to_start_of_array(array, element)
