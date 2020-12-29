810eec448ebc74396edc61722333585cacd8c0a9
Blaine R Love <blainelove94@gmail.com>
2020-12-29 20:58:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,11 +4,10 @@ end
 
 def create_an_array
   [0, 1, 2, 3 ]
-
 end
 
 def add_element_to_end_of_array(array, element)
-
+  add_element_to_end_of_array.push(1)
 end
 
 def add_element_to_start_of_array(array, element)
