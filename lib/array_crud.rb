c55db8a1f4a02d10edfec4150cde94c4083c1f98
Blaine R Love <blainelove94@gmail.com>
2020-12-29 21:03:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [0, 1, 2, 3 ]
+  bill = [0, 1, 2, 3 ]
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push("item")
+  bill.push("item")
 end
 
 def add_element_to_start_of_array(array, element)
